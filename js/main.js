13c2bec040a1b4d1f577a29bb91914f4cc790e92
Dante3056 <ddscodemail@gmail.com>
2021-05-18 17:46:04 +0200

    entrega de ejercicio de validaciones en javascript, nivel 1 y 2

---
+++ js/main.js
@@ -0,0 +1,96 @@
+//variables
+const formulario = document.getElementsByClassName('formulario');
+const inputs = document.querySelectorAll('.formulario input');
+let pass1 = document.getElementById('inputPassword_2');
+let pass2 = document.getElementById('inputPassword_3');
+let provincia = document.getElementById('inputProvince');
+let searchBar = document.getElementById('inputSearch');
+let regex = {
+    correo: /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/,
+    password: /^.*(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/, //Almenos 8 caracteres una mayúscula y un número (Nivel 2)
+    required: /(?=.{3,})(?=.*[a-zA-Z0-9])///Almenos 3 letras como mínimo para iniciar la búsqueda.
+}
+//eventos: eventos submit
+document.querySelectorAll('.formulario').forEach(item => {
+    item.addEventListener('submit', event => {
+        validarFormulario;
+        for (i = 0; i < inputs.length; i++) {
+            if (inputs[i].value === '' || inputs[i].value === false) {
+                inputs[i].classList.add('is-invalid');
+                provincia.classList.add('is-invalid');
+            }
+        }
+        event.preventDefault();        
+    })
+})
+
+//limpiar clases
+document.querySelectorAll('.formulario').forEach(item => {
+        item.addEventListener('focusout', () => {
+        for (i = 0; i < inputs.length; i++) {
+           inputs[i].classList.remove('is-invalid');
+           provincia.classList.remove('is-invalid');
+        }
+    })
+})
+//evento para validar campos
+inputs.forEach((input) => {
+    input.addEventListener('keyup', validarFormulario);
+    input.addEventListener('blur', validarFormulario);
+    provincia.addEventListener('change', validarFormulario);
+    provincia.addEventListener('blur', validarFormulario);
+    searchBar.addEventListener('change', validarFormulario);
+    searchBar.addEventListener('blur', validarFormulario);
+    searchBar.addEventListener('keyup', validarFormulario);
+    searchBar.addEventListener('focusout', resetForm);
+})
+
+//funciones:Validacion de campos
+function validarFormulario(e) {
+    switch (e.target.name) {
+        case 'correoLogin':
+            validarCampo(regex.correo, e.target);
+            document.getElementById('errorMail').textContent = "introduce una dirección de correo válida";
+            break;
+        case 'correoSignup':
+            validarCampo(regex.correo, e.target);
+            document.getElementById('errorMail').textContent = "introduce una dirección de correo válida";
+            break;
+        case 'loginPass':
+            validarCampo(regex.password, e.target);
+            document.getElementById('errorPassword').textContent = "la contraseña no concuerda";
+            break;
+        case 'signupPass':
+            validarCampo(regex.password, e.target);
+            document.getElementById('errorPassword').textContent = "la contraseña no concuerda";
+            break;
+        case 'signupPass2':
+            pass2.value !== pass1.value ? pass2.classList.add('is-invalid') : pass2.classList.remove('is-invalid');
+            document.getElementById('errorPassword_2').textContent = "la contraseña no concuerda";
+            break;
+        case 'provincia':
+            validarCampo(regex.required, e.target);
+            document.getElementById('errorProv').textContent = "el campo es obligatorio";
+            break;
+        case 'search':
+            validarCampo(regex.required, e.target);
+            document.getElementById('errorSearch').textContent = "Tu búsqueda debe contener almenos 3 caracteres";
+            break;    
+    }
+}
+
+//prueba de campo valido usando expresiones regulares
+const validarCampo = (expresion, input) => {
+    if (expresion.test(input.value) == false) {
+        input.classList.add('is-invalid');
+    } else {
+        input.classList.remove('is-invalid');
+    }
+}
+
+//funcion para resetear clases
+function resetForm() {
+    searchBar.classList.remove('is-invalid')
+    document.classList.remove('is-invalid');
+}
+
